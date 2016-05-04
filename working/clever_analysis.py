b7431640504feefc1cc88852e5822b6976132c03
Matthew Brett <matthew.brett@gmail.com>
2016-05-04 19:50:53 -0400

    my-snapshot-7

--- working/clever_analysis.py
+++ working/clever_analysis.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 FUDGE = 42
+HOT_FUDGE = 1.707
 
 # Load data from the brain
 data = np.loadtxt('expensive_data.csv', delimiter=',')
@@ -13,6 +14,7 @@ from_world, from_brain = data.T
 
 # Process data
 from_brain_processed = np.log(from_brain) * FUDGE * np.e ** np.pi
+from_brain_processed = from_brain_processed / HOT_FUDGE
 
 # Make plot
 plt.plot(from_world, from_brain_processed, 'r:')
